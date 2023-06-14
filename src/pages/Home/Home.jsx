469823ade09a95603184f2ffb6bf4dfdc3a96576
seito-developer <seito.horiguchi@gmail.com>
2023-06-15 02:27:33 +0900

    replaced to API

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,34 +5,26 @@ import Board from "../../components/Board/Board";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const boards = [
-    {
-        id: 1,
-        title: "Weekly tasks"
-    }, {
-        id: 2,
-        title: "Private tasks"
-    }
-];
-
 const Home = () => {
-    const [boardsData, setBoardsData] = useState(null);
+    const [boardsData, setBoardsData] = useState([]);
 
     useEffect(()=>{
         const fetch = async () => {
-            const res = await axios.get('http://localhost:3000/boards');
-            console.log('res:', res);
-            return res;
+            try {
+                const res = await axios.get('http://localhost:3000/boards');
+                setBoardsData(res.data);
+            } catch (error) {
+                console.log('error:', error);
+            }
         }
-        setBoardsData(fetch());
-        
+        fetch();
     },[]);
 
     return (
         <Layout title={"ToDo App"}>
             <div className="page-home">
                 <div className="page-home__contents">
-                    { boards.map((board, index) => {
+                    { boardsData.map((board, index) => {
                         return <Board key={index} {...board} />
                     })}
                     <AddBoard />
